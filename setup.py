37bf717c5655d97bbc228b7324b1061f7ed33c16
PythonScratcher <81612672+PythonScratcher@users.noreply.github.com>
2024-07-08 10:16:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sha69',
+    version='0.1',
+    packages=find_packages(),
+    description='Custom hashing algorithm (SHA-69) implementation',
+    author='Your Name',
+    author_email='mc@psr.tf',
+    license='MIT',
+    keywords='hashing sha-69',
+    url='https://github.com/PythonScratcher/SHA-69',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
